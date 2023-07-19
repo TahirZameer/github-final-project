6d2f7c92cbe17abc9bd4b99363e6fb1e898d4562
Tahir Zameer <Tahirzameer.2000@gmail.com>
2023-07-19 15:40:34 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # TahirZameer
+   # <your GitHub username>
    # Input:
    # p, principal amount
    # t, time period in years
